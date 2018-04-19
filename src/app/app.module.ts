1c928451879a4dae5006f72f85bd1c3757715a29
keyurlakhani <keyur.lakhani99@gmail.com>
2018-04-18 17:38:26 -0700

    Reactive Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '' , component:SignupFormComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
